ed96b07044119b4c6e94b18de16d179fc67725e5
rampKKJ <107202129+rampKKJ@users.noreply.github.com>
2023-12-08 10:34:33 -0300

    bugre

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-function tocaSom(idElementoAudio){
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-}
+    }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
-
-for (let conrador = 0; contador < listaDeTeclas.length; contador++) {
-    const tecla = listaDeTeclas[contador];
+    const listaDeTeclas = document.querySelectorAll(".tecla");
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas [contador];
     const efeito = tecla.classList[1];
-    const idAudio = '#som_${efeito}';
+    const idAudio = `#som ${efeito}`;
     tecla.onclick = function() {
-        tocaSom(idAudio);
+    tocaSom (idAudio);
     };
-}
+    }
